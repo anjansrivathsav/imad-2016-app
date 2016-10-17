88f247611168123e87037738d781b27d2e7f7cb4
anjansrivathsav <anjansrivathsav1997@gmail.com>
2016-10-17 23:35:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-txt');
 element.innerHTML='new value';
-var img =document.getElementById('madi');
+var img =document.getElementById('anji');
 var marginLeft=0;
 function moveright(){
   marginLeft=marginLeft+10;
